455617605a5eec4bb72cd51010b4c53661950fdf
Supergamerx3000 <61492037+Supergamerx3000@users.noreply.github.com>
2022-06-21 07:59:04 +0200

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -3,7 +3,7 @@ if zenity --question --ellipsize --title="Update Nitrox" \
     --text="Do you wish to update nitrox?\nThis may overwrite some seting in the Nitrox Launcher\n(such as Subnautica game folder)"
 then
     {
-        wget -O nitrox.zip https://on.rux.gg/6eRNs
+        wget -O nitrox.zip https://github.com/SubnauticaNitrox/Nitrox/releases/download/1.5.0.1/Nitrox.1.5.0.1.zip
         echo 50
         unzip -o -q nitrox.zip -d "drive_c/Program Files/Nitrox/"
         echo 100
